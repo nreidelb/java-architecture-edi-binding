88ce14ae5cb11a97d6611b7cfedec64db4701194
nreidelb <nreidelb@redhat.com>
2014-01-15 14:59:48 -0500

    Update Header.java

--- jaeb-model-x12/src/main/java/javax/edi/model/x12/edi855/segment/Header.java
+++ jaeb-model-x12/src/main/java/javax/edi/model/x12/edi855/segment/Header.java
@@ -1,6 +1,11 @@
 package javax.edi.model.x12.edi855.segment;
 
+import java.util.Collection;
+import javax.edi.bind.annotations.EDICollectionType;
 import javax.edi.bind.annotations.EDISegmentGroup;
+import javax.edi.model.x12.segment.LineItemAcknowledgement;
+import javax.edi.model.x12.segment.ReferenceIdentification;
+import javax.edi.model.x12.segment.ReferenceNumber;
 import javax.edi.model.x12.segment.TransactionSetHeader;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -15,6 +20,9 @@ public class Header implements javax.edi.model.x12.Header {
 	@NotNull
 	@Valid
 	private POAcknowledgementBeginningSegment beginningSegment;
+        
+        @EDICollectionType(ReferenceIdentification.class)
+        private Collection<ReferenceIdentification> referenceIdentification;
 
 	public TransactionSetHeader getTransactionSetHeader() {
 		return transactionSetHeader;
@@ -32,6 +40,20 @@ public class Header implements javax.edi.model.x12.Header {
 			POAcknowledgementBeginningSegment beginningSegment) {
 		this.beginningSegment = beginningSegment;
 	}
+
+    /**
+     * @return the referenceIdentification
+     */
+    public Collection<ReferenceIdentification> getReferenceIdentification() {
+        return referenceIdentification;
+    }
+
+    /**
+     * @param referenceIdentification the referenceIdentification to set
+     */
+    public void setReferenceIdentification(Collection<ReferenceIdentification> referenceIdentification) {
+        this.referenceIdentification = referenceIdentification;
+    }
 	
 	
 }
