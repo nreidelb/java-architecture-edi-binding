0610f60fbcbfa0714556a943a6c4d9c753bd84a4
nreidelb <nreidelb@redhat.com>
2014-05-08 17:28:24 -0400

    Update Trailer.java

--- jaeb-model-x12/src/main/java/javax/edi/model/x12/edi856/segment/Trailer.java
+++ jaeb-model-x12/src/main/java/javax/edi/model/x12/edi856/segment/Trailer.java
@@ -1,25 +1,14 @@
 package javax.edi.model.x12.edi856.segment;
 
 import javax.edi.bind.annotations.EDISegmentGroup;
-import javax.edi.model.x12.segment.TransactionSetTotals;
 import javax.edi.model.x12.segment.TransactionSetTrailer;
 import javax.validation.constraints.NotNull;
 
 @EDISegmentGroup
 public class Trailer {
-
-	private TransactionSetTotals transactionSetTotals;
 	
 	@NotNull
 	private TransactionSetTrailer transactionSetTrailer;
-
-	public TransactionSetTotals getTransactionSetTotals() {
-		return transactionSetTotals;
-	}
-
-	public void setTransactionSetTotals(TransactionSetTotals transactionSetTotals) {
-		this.transactionSetTotals = transactionSetTotals;
-	}
 
 	public TransactionSetTrailer getTransactionSetTrailer() {
 		return transactionSetTrailer;
